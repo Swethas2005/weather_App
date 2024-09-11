139ed235ea09ef0ae45bb4b6119b35a722cbbc88
Swethas2005 <swethas15092005@gmail.com>
2024-09-11 18:25:51 +0530

    weather_App

---
+++ weatherApp/src/components/Map.jsx
@@ -0,0 +1,15 @@
+export const Map = ({ city }) => {
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height="300"
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        // src={`https://www.google.com/maps/embed/v1/place?q=${city}`}
+        src={`https://maps.google.com/maps?q=${city}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+      ></iframe>
+    </div>
+  );
+};
